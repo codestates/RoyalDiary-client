72e9b5e2bd1b329fa708d0291e1b6820c109b6e3
JayKim <JayKim@users.noreply.github.com>
2021-02-23 00:27:42 +0900

    [client]ADD manual page 개발
    서비스 사용법을 공지하는 manual page 개발

--- client-app/src/components/manual.tsx
+++ client-app/src/components/manual.tsx
@@ -1,3 +1,17 @@
-import React from "react";
+import React, { ReactElement } from "react";
+import styled, { keyframes } from "styled-components";
 
-export {};
+export default function Cmanual(): ReactElement {
+	const Main = styled.div`
+		// border: 10px solid black;
+		position: relative;
+		left: -2rem;
+		bottom: 1rem;
+		margin: auto;
+		display: flex;
+		height: 25%;
+		width: 80%;
+	`;
+
+	return <Main />;
+}
