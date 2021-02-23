7bbc083e5babd8ee3dadac738cb6e84ab71a71f8
atomsong92 <yesparrot@gmail.com>
2021-02-23 14:03:25 +0900

    [client] ADD 캘린더 페이지 개발(중간저장)
    캘린더 페이지 개발

--- client-app/src/pages/calendar.tsx
+++ client-app/src/pages/calendar.tsx
@@ -2,7 +2,18 @@ import React, { ReactElement } from "react";
 import styled, { keyframes } from "styled-components";
 
 export default function CalendarRows(): ReactElement {
-	const Main = styled.div``;
+	const Main = styled.div`
+		background: pink;
+		width: 50%;
+	`;
 
-	return <Main />;
+	const A = styled.div`
+		border: 1px solid black;
+	`;
+
+	return (
+		<Main>
+			<A>dd</A>
+		</Main>
+	);
 }
