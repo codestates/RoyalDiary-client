12ebf4244170b216279ca5e1386b80d8d11414bf
atomsong92 <yesparrot@gmail.com>
2021-02-23 14:08:22 +0900

    [server] ADD 일기보기 페이지,컴포넌트 개발
    일기보기 페이지,컴포넌트 개발

--- client-app/src/components/diaryCard.tsx
+++ client-app/src/components/diaryCard.tsx
@@ -2,26 +2,20 @@ import React from "react";
 import styled from "styled-components";
 
 function DiaryCard() {
-	const Card = styled.div`
-		margin-left: 10px;
+	const Card = styled.span`
+		height: 28%;
+		margin-left: 0.1rem;
+		margin-top: 0.5rem;
 	`;
-	const DiaryCreatedAt = styled.h2`
-		width: 200px;
-		justify-content: center;
-		display: flex;
-		margin-bottom: 5px;
+	const DiaryCreatedAt = styled.h3`
+		margin: 0rem 2.5rem;
 	`;
 
 	const DiaryCardContent = styled.div`
-		width: 200px;
-		justify-content: center;
-		display: flex;
-		font-size: 1.2em;
-		padding: 0px;
+		margin: 0rem 2.5rem;
 	`;
 	const Diaryimage = styled.div`
 		border: 1px solid black;
-		width: 200px;
 	`;
 
 	return (
@@ -30,13 +24,12 @@ function DiaryCard() {
 			<Diaryimage>
 				<img
 					src="https://image-storage-homemade.s3.ap-northeast-2.amazonaws.com/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7+2021-02-21+%EC%98%A4%ED%9B%84+2.21.11.png"
-					width="200px"
-					height="200px"
+					width="180px"
+					height="180px"
 					alt=""
 				/>
 			</Diaryimage>
-
-			<DiaryCardContent className="writer">작성자 : 송정현 </DiaryCardContent>
+			<DiaryCardContent>작성자 : 송정현 </DiaryCardContent>
 		</Card>
 	);
 }
