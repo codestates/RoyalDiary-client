667cee6b64c40a974331874c17aee7cf26647390
atomsong92 <yesparrot@gmail.com>
2021-02-24 11:12:33 +0900

    [client] ADD calendar 컴포넌트 개발
    컴포넌트 개발

--- client-app/src/components/calendar.tsx
+++ client-app/src/components/calendar.tsx
@@ -1,3 +1,26 @@
-import React from "react";
+import React, { ReactElement, useState } from "react";
+import styled, { keyframes } from "styled-components";
+import "react-calendar/dist/Calendar.css";
+import ReactCalendar from "react-calendar";
+import "./a.css";
 
-export {};
+export default function Calendar(): ReactElement {
+	const Date = styled.div`
+		margin-top: 10%;
+		margin-left: 10%;
+		height: 35%;
+		width: 80%;
+		display: flex;
+		justify-content: center;
+	`;
+
+	const style1 = {
+		fontSize: "15px",
+	};
+
+	return (
+		<Date>
+			<ReactCalendar className="main" />
+		</Date>
+	);
+}
