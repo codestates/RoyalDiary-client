abea21e99e92fbf915ea398df951219a80fecb59
JayKim <JayKim@users.noreply.github.com>
2021-03-04 01:25:41 +0900

    [client]ADD paint component 로직 구현
    그림판 적용(색상변경, 선굵기변경, 수정가능) 및 완료 버튼 클릭시
    PNG 이미지 파일을 S3버킷에 업로드할 수 있도록 로직 구현
    이미지 url과 좌표데이터(이미지 수정시 필요)를 데이터베이스에 저장할 예정

--- client-app/src/pages/paint.tsx
+++ client-app/src/pages/paint.tsx
@@ -4,76 +4,53 @@ import Diaryinfo from "../components/diaryInfo";
 import CPaint from "../components/paint";
 
 export default function Paint(): ReactElement {
-	const Main = styled.div`
-		background: #f6f6ee;
-		border-right: 5px solid black;
-		box-sizing: border-box;
-		width: 50%;
-		height: 100%;
-		display: flex;
-		flex-direction: column;
-		flex-grow: 1;
-		@media only screen and (max-width: 1200px) {
-			width: 80%;
-			height: 100%;
-			border-right: none;
-			border-bottom: 5px solid black;
-		}
-		@media only screen and (max-width: 480px) {
-			/* margin-left: 1rem; */
-			height: 70%;
-		}
-	`;
-	const Paintbox = styled.div`
-		border: 5px solid black;
-		border-radius: 2rem;
-		margin: 3rem 2rem 3rem 2rem;
-		height: 90%;
-		display: flex;
-		flex-direction: column;
-		@media only screen and (max-width: 480px) {
-			/* margin-left: 1rem; */
-			margin: 2rem 1rem 2rem 1rem;
-		}
-	`;
-	const Title = styled.div`
-		// border: 3px solid black;
-		margin-top: 0.5rem;
-		padding-left: 1rem;
-		font-size: 1.2rem;
-		font-weight: bold;
-		letter-spacing: 0.2rem;
-	`;
-	const Buttons = styled.div`
-		// border: 5px solid black;
-		margin-bottom: 2rem;
-		padding-right: 1rem;
-		flex-grow: 0.2;
-		display: flex;
-		flex-direction: row;
-		// align-items: right;
-		justify-content: flex-end;
-	`;
-	const Button = styled.button`
-		// border: 1px solid black;
-		background: #f6f6ee;
-		margin-right: 1rem;
-		font-size: 1rem;
-		font-weight: bold;
-		height: 80%;
-	`;
-
 	return (
 		<Main>
 			<Paintbox>
 				<Diaryinfo />
 				<Title>😍(유저)의 행복한 하루😍</Title>
 				<CPaint />
-				<Buttons>
-					<Button>새종이</Button>
-					<Button>다그렸다버튼</Button>
-				</Buttons>
 			</Paintbox>
 		</Main>
 	);
 }
+const Main = styled.div`
+	background: #f6f6ee;
+	border-right: 5px solid black;
+	box-sizing: border-box;
+	width: 50%;
+	height: 100%;
+	display: flex;
+	flex-direction: column;
+	flex-grow: 1;
+	@media only screen and (max-width: 1200px) {
+		width: 80%;
+		height: 100%;
+		border-right: none;
+		border-bottom: 5px solid black;
+	}
+	@media only screen and (max-width: 480px) {
+		/* margin-left: 1rem; */
+		height: 70%;
+	}
+`;
+const Paintbox = styled.div`
+	border: 5px solid black;
+	border-radius: 2rem;
+	margin: 3rem 2rem 3rem 2rem;
+	height: 90%;
+	display: flex;
+	flex-direction: column;
+	@media only screen and (max-width: 480px) {
+		/* margin-left: 1rem; */
+		margin: 2rem 1rem 2rem 1rem;
+	}
+`;
+const Title = styled.div`
+	// border: 3px solid black;
+	margin-top: 0.5rem;
+	padding-left: 1rem;
+	font-size: 1.2rem;
+	font-weight: bold;
+	letter-spacing: 0.2rem;
+`;
