72763180f631f537264fabd515f0a33e6ce905bb
JayKim <JayKim@users.noreply.github.com>
2021-03-14 15:29:44 +0900

    [client]ADD 개발자 소개 페이지 구현
    개발자 소개 페이지 구현(팀원 소개, 깃헙/블로그 주소 컴포넌트로 구성)

---
+++ client-app/src/components/developerTeam.tsx
@@ -0,0 +1,174 @@
+import React, { ReactElement } from "react";
+import styled from "styled-components";
+import song from "../assets/images/developers/song.jpg";
+import kim from "../assets/images/developers/kim.jpg";
+import lee from "../assets/images/developers/lee.jpg";
+import sin from "../assets/images/developers/sin.jpg";
+
+export default function DeveloperTeam(): ReactElement {
+	return (
+		<Main>
+			<Body>
+				<Paintbox>
+					<Paint>
+						<MemberBox>
+							<EmojiImg src={song} />
+							<IntroMember>
+								<b>팀장 송정현</b>
+								<br />
+								프론트엔드 개발
+								<br />: 나의일기, 훔쳐보기, 댓글페이지, 마이페이지 디자인 및 로직 구현
+							</IntroMember>
+						</MemberBox>
+						<MemberBox>
+							<EmojiImg src={kim} />
+							<IntroMember>
+								<b>멤버 김용재</b>
+								<br />
+								프론트엔드 개발
+								<br />: 로그인, 회원가입, 일기쓰기, 제작자 페이지 디자인 및 로직 구현
+							</IntroMember>
+						</MemberBox>
+						<MemberBox>
+							<EmojiImg src={lee} />
+							<IntroMember>
+								<b>멤버 이태경</b>
+								<br />
+								백엔드 개발
+								<br />: 로그인, 회원가입, 마이페이지 API 구현, DB구축, 배포 담당
+							</IntroMember>
+						</MemberBox>
+						<MemberBox>
+							<EmojiImg src={sin} />
+							<IntroMember>
+								<b>멤버 신예담</b>
+								<br />
+								백엔드 개발
+								<br />: 일기쓰기, 훔쳐보기, 나의일기, 댓글페이지 API 구현
+							</IntroMember>
+						</MemberBox>
+					</Paint>
+				</Paintbox>
+				<TitleContent>
+					<Title>프로젝트: Royal Diary - 그림일기 웹 서비스</Title>
+					<Content>
+						우리들의 추억속으로 여러분을 초대합니다.
+						<br />
+						신나는 여름방학 늘 함께한 추억의 그림일기장! <br />
+						매일 그림 그리고, 일기를 쓰며 마음 건강을 챙기세요.
+						<br />
+						원하는 그림일기 내용을 가족, 친구, 사람들과 공유하여 작은 위로를 받을 수도 있어요.
+						<br />
+						우리들의 소중한 일상을 그림 일기에 채워나가세요 :)
+					</Content>
+				</TitleContent>
+			</Body>
+		</Main>
+	);
+}
+const Main = styled.div`
+	/* border: 3px solid red; */
+	background: #f6f6ee;
+	padding-top: 1rem;
+	box-sizing: border-box;
+	display: flex;
+	height: 100%;
+	flex-direction: column;
+`;
+const Body = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-grow: 2;
+`;
+const Paintbox = styled.div`
+	/* border: 5px solid black; */
+	display: flex;
+	flex-direction: column;
+	height: 50%;
+	@media only screen and (max-width: 1100px) {
+		height: 70%;
+		flex-direction: column;
+	}
+	@media only screen and (max-width: 480px) {
+		height: 50%;
+	}
+`;
+const Paint = styled.div`
+	border: 3px solid black;
+	margin: auto;
+	width: 90%;
+	height: 100%;
+	background-color: white;
+	display: flex;
+	@media only screen and (max-width: 1100px) {
+		flex-grow: 0.3;
+		flex-direction: column;
+	}
+	@media only screen and (max-width: 520px) {
+		flex-grow: 1;
+	}
+	@media only screen and (max-width: 480px) {
+		height: 200%;
+	}
+`;
+const MemberBox = styled.div`
+	/* border: 3px solid red; */
+	width: 95%;
+	padding-left: 0.5rem;
+	display: flex;
+	flex-direction: column;
+	@media only screen and (max-width: 1100px) {
+		flex-direction: row;
+	}
+`;
+const EmojiImg = styled.img`
+	/* border: 3px solid blue; */
+	width: 100%;
+	@media only screen and (max-width: 1100px) {
+		width: 12%;
+	}
+	@media only screen and (max-width: 520px) {
+		width: 20%;
+	}
+`;
+const IntroMember = styled.div`
+	/* border: 3px solid green; */
+	height: 40%;
+	padding-left: 0.2rem;
+`;
+const TitleContent = styled.div`
+	/* border: 3px solid yellow; */
+	flex-grow: 1;
+	margin-top: 1rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+`;
+const Title = styled.div`
+	/* border: 3px solid red; */
+	margin: 0rem 3rem;
+	font-size: 1.2rem;
+	font-weight: bold;
+	letter-spacing: 0.2rem;
+	@media only screen and (max-width: 570px) {
+		font-size: 1rem;
+	}
+	@media only screen and (max-width: 480px) {
+		display: none;
+	}
+`;
+const Content = styled.div`
+	/* border: 1px solid black; */
+	margin: 0rem 3rem;
+	padding-top: 0.7rem;
+	line-height: 2rem;
+	font-size: 1.1rem;
+	display: flex;
+	height: 12rem;
+	@media only screen and (max-width: 570px) {
+		font-size: 0.9rem;
+	}
+	@media only screen and (max-width: 480px) {
+		display: none;
+	}
+`;
---
+++ client-app/src/pages/developer.tsx
@@ -0,0 +1,242 @@
+import React, { ReactElement, useState } from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+import laptop from "../assets/images/menuLogos/black-laptop.png";
+import github from "../assets/images/menuLogos/github.png";
+import mail from "../assets/images/menuLogos/mail.png";
+import DeveloperTeam from "../components/developerTeam";
+import DeveloperGithub from "../components/developerGithub";
+import DeveloperMail from "../components/developerMail";
+
+export default function Developer(): ReactElement {
+	const history = useHistory();
+	const [message, setMessage] = useState("");
+	const [msgVisible, setMsgVisible] = useState(false);
+	const [currentPage, setCurrentPage] = useState("laptop");
+	const changePage = (e: any) => {
+		const selectedPage = e.target.id;
+		setCurrentPage(selectedPage);
+	};
+	const submitMsg = () => {
+		setMessage("마음만으로도 감사합니다😚");
+		setMsgVisible(true);
+		setTimeout(() => {
+			setMsgVisible(false);
+		}, 2000);
+	};
+	return (
+		<EntirePage>
+			<Main>
+				<Header>
+					<Team>About Team Summer Vacation😍</Team>
+					<TeamIntro>
+						<div>IM25 Final Project Team</div>
+					</TeamIntro>
+					<Menus>
+						<Image>
+							<IconImage id="laptop" src={laptop} onClick={changePage} />
+						</Image>
+						<Image>
+							<IconImage id="github" src={github} onClick={changePage} />
+						</Image>
+						<Image>
+							<IconImage id="mail" src={mail} onClick={changePage} />
+						</Image>
+					</Menus>
+				</Header>
+				<Body>
+					{currentPage === "laptop" ? <DeveloperTeam /> : null}
+					{currentPage === "github" ? <DeveloperGithub /> : null}
+					{currentPage === "mail" ? <DeveloperMail /> : null}
+				</Body>
+				<div id="wrap">
+					<ValidityBox theme={msgVisible}>{message}</ValidityBox>
+				</div>
+				<Buttons>
+					<Button onClick={() => history.push("/")}>확인버튼</Button>
+					{currentPage === "mail" ? <Button onClick={submitMsg}>후원하기</Button> : null}
+				</Buttons>
+			</Main>
+		</EntirePage>
+	);
+}
+const EntirePage = styled.div`
+	/* border: 3px solid rgb(0, 0, 255); */
+	width: 50%;
+	height: 100%;
+	@media only screen and (max-width: 1100px) {
+		width: 100%;
+	}
+`;
+const Main = styled.div`
+	background: #f6f6ee;
+	border: 3px solid black;
+	box-sizing: border-box;
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	div#wrap {
+		/* border: 3px solid green; */
+		height: 2rem;
+		margin-top: 0.5rem;
+		padding-right: 1rem;
+		display: flex;
+		justify-content: flex-end;
+	}
+	@media only screen and (max-width: 1100px) {
+		width: 100%;
+	}
+	@media only screen and (max-width: 480px) {
+		min-width: 400px;
+		height: 70%;
+	}
+`;
+const Header = styled.div`
+	/* border: 3px solid black; */
+	display: flex;
+	flex-direction: column;
+	text-align: center;
+	@media only screen and (max-width: 480px) {
+		min-height: 10rem;
+	}
+`;
+const Team = styled.div`
+	border-bottom: 3px solid black;
+	height: 3rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 1.7rem;
+	@media only screen and (max-width: 480px) {
+		font-size: 1.2rem;
+		padding: 0.5rem;
+		height: 2rem;
+	}
+`;
+const TeamIntro = styled.div`
+	border-bottom: 3px solid black;
+	display: flex;
+	padding: 0.3rem 0.7rem;
+	height: 2rem;
+	justify-content: center;
+	align-items: center;
+	font-size: 1.2rem;
+	/* font-weight: bold; */
+	letter-spacing: 0.5rem;
+	@media (max-width: 480px) {
+		font-size: 1rem;
+		padding-left: 1rem;
+		letter-spacing: 0.4rem;
+	}
+`;
+const Menus = styled.div`
+	/* height: 5rem; */
+	border-bottom: 3px solid black;
+	height: 4rem;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	flex-wrap: wrap;
+	@media only screen and (max-width: 480px) {
+		width: 100%;
+		height: 30%;
+		margin-bottom: 0.5%;
+	}
+`;
+const Body = styled.div`
+	/* border: 3px solid red; */
+	display: flex;
+	flex-direction: column;
+	flex-grow: 2;
+`;
+const Image = styled.div`
+	/* border: 3px solid red; */
+	width: 4rem;
+	/* height: 4rem; */
+	display: flex;
+	justify-content: center;
+`;
+const IconImage = styled.img`
+	/* border: 3px solid black; */
+	width: 3rem;
+	alt: "";
+	:hover {
+		cursor: pointer;
+	}
+	@media only screen and (max-width: 480px) {
+		width: 2.5rem;
+	}
+`;
+const Buttons = styled.div`
+	/* border: 5px solid black; */
+	/* flex-grow: 0.5; */
+	padding-right: 1rem;
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: flex-end;
+	@media only screen and (max-width: 480px) {
+		padding-right: 2rem;
+	}
+`;
+const Button = styled.button`
+	border: 2px solid black;
+	background: #f6f6ee;
+	margin-right: 1rem;
+	margin-bottom: 1rem;
+	font-size: 1rem;
+	font-weight: bold;
+	display: flex;
+	:hover {
+		cursor: pointer;
+		background: black;
+		color: white;
+	}
+	@media only screen and (max-width: 480px) {
+		/* margin-left: 1rem; */
+		font-size: 0.7rem;
+		/* width: 5rem; */
+		height: 1.5rem;
+		margin-right: 0.5rem;
+	}
+`;
+const ValidityBox = styled.div`
+	/* border: 3px solid red; */
+	position: relative;
+	height: 30px;
+	padding: 0.2rem 1rem 0.2rem 1rem;
+	display: ${(props) => (props.theme === true ? "flex" : "none")};
+	border-radius: 10px;
+	justify-content: flex-start;
+	align-items: center;
+	::after {
+		border-top: 0px solid transparent;
+		border-left: none;
+		border-right: none;
+		content: "";
+		position: absolute;
+		top: -10px;
+		left: 120px;
+	}
+	animation: a 2s;
+	@keyframes a {
+		0% {
+			opacity: 0;
+		}
+		100% {
+			opacity: 1;
+		}
+	}
+	@media only screen and (max-width: 480px) {
+		::after {
+			border-top: 0px solid transparent;
+			border-left: none;
+			border-right: none;
+			border-bottom: 10px solid #f08080;
+			content: "";
+			position: absolute;
+			top: -10px;
+			left: 120px;
+		}
+	}
+`;
